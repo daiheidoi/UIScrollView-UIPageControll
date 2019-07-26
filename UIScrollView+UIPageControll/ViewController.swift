a60531fd9ac6b95704cb9997de19232cafe0e951
daiheidoi <doi_daihei@andfactory.co.jp>
2019-07-26 17:34:14 +0900

    Initial Commit

---
+++ UIScrollView+UIPageControll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIScrollView+UIPageControll
+//
+//  Created by doi on 2019/07/26.
+//  Copyright © 2019 doi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
