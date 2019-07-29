0250d1a28723f74792ac4dbc9f68162d96a68a83
daiheidoi <doi_daihei@andfactory.co.jp>
2019-07-29 13:48:55 +0900

    リファクタ

--- UIScrollView+UIPageControll/ViewController.swift
+++ UIScrollView+UIPageControll/ViewController.swift
@@ -20,16 +20,42 @@ final class ViewController: UIViewController {
         }
     }
     
+    @IBOutlet private weak var nextButton: UIButton!
+    
     private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        collectionView.rx.didEndDecelerating
-            .withLatestFrom(collectionView.rx.contentOffset)
-            .map { Int($0.x / UIScreen.main.bounds.width) }
-            .bind(to: pageControll.rx.currentPage)
+    
+        Observable.just(Const.lotties.count)                    // ページ数
+            .bind(to: pageControll.rx.numberOfPages)            // ページコントロールにbind
             .disposed(by: disposeBag)
+        
+        collectionView.rx.didScroll                             // スクロール位置変更を流す
+            .withLatestFrom(collectionView.rx.contentOffset)    // 最新値contentOffset
+            .map { Int($0.x / UIScreen.main.bounds.width) }     // ページ番号に加工
+            .bind(to: pageControll.rx.currentPage)              // ページコントロールの現在ページにバインド
+            .disposed(by: disposeBag)
+        
+        nextButton.rx.tap
+            .filter { [unowned self] in
+                self.pageControll.currentPage < (Const.lotties.count - 1) // ページ数超えないようfilter
+            }
+            .subscribe(onNext: { [unowned self] _ in
+                self.collectionView.setContentOffset(CGPoint(x: self.collectionView.contentOffset.x + UIScreen.main.bounds.width, y: 0), animated: true)  // スクロール移動
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
+extension ViewController {
+    
+    struct Const {
+        static let lotties = [
+            "test0",
+            "test1",
+            "test2"
+        ]
     }
 }
 
@@ -37,12 +63,12 @@ final class ViewController: UIViewController {
 extension ViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return Const.lotties.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: PageItemCell =  collectionView.dequeueReusableCell(for: indexPath)
-        cell.setData(lottieFile: "test\(indexPath.row)")
+        cell.setData(lottieFile: Const.lotties[indexPath.item])
         return cell
     }
 }
